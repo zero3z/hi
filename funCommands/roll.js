3225a2e7d7280814120f007b45f9d6ccf43e2669
zaooo207 <cailc2364@gmail.com>
2024-06-16 05:09:52 +0700

    Update roll.js

--- funCommands/roll.js
+++ funCommands/roll.js
@@ -4,7 +4,16 @@ module.exports = {
   aliases: ['rd', 'roll'],
   description: 'Random',
   execute(message, args) { 
-    const number = Math.floor(Math.random() * 100) + 1;
-    message.channel.send(`Số random là: ${number}`);
+    if (args.length === 1) {
+            const max = parseInt(args[0]);
+
+            if (!isNaN(max) && max > 0) {
+                const number = Math.floor(Math.random() * max) + 1;
+                message.channel.send(`Your random number between 1 and ${max} is: ${number}`);
+            } else {
+                message.channel.send('Please provide a valid positive number.');
+            }
+        } else {
+            message.channel.send('Usage: !random <max>');
   },
 };
