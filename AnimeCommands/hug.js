f04625d17a1a44bdab126e19ed26c7648083b6c1
zaooo207 <cailc2364@gmail.com>
2024-06-15 16:56:04 +0700

    Update hug.js

--- AnimeCommands/hug.js
+++ AnimeCommands/hug.js
@@ -3,7 +3,7 @@ const anime = require('anime-actions');
 const db = require("../mongodb");
 module.exports = {
   name: 'hug',
-  description: 'Send a hug gif in an embedded message!',
+  description: 'ôm',
   async execute(message, args) {
     
     const sender = message.author;
@@ -17,7 +17,7 @@ module.exports = {
  
     const embed = new EmbedBuilder()
       .setColor('#ff0000')
-      .setDescription(`${sender} sends a hug to ${targetUser}!`)
+      .setDescription(`${sender} đã ôm ${targetUser}!`)
       .setImage(hugGif);
 
     
