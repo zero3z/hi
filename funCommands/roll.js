53907d6dbae30a87926cfb89f2952a244e5a060d
zaooo207 <cailc2364@gmail.com>
2024-06-16 03:26:28 +0700

    Update roll.js

--- funCommands/roll.js
+++ funCommands/roll.js
@@ -11,6 +11,6 @@ module.exports = {
     }
     const random = parseInt(args[0])
     let rd = Math.floor(Math.random() * random) + 1;
-    await message.reply('**Con số random của ${message.member} là:** __**${rd}**__ `);
+    message.channel.send('**Con số random của ${message.member} là:** __**${rd}**__ `);
   },
 };
